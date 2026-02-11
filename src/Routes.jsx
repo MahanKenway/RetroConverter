15dfd79a30d9e71b9cedb815e6ddd49da7a8469e
Mahan Tavakoli <94042573+MahanKenway@users.noreply.github.com>
2026-02-11 20:26:35 +0330

    Create Routes.jsx

---
+++ src/Routes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+import NotFound from "pages/NotFound";
+import Windows98DesktopInterface from './pages/windows-98-desktop-interface';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your route here */}
+        <Route path="/" element={<Windows98DesktopInterface />} />
+        <Route path="/windows-98-desktop-interface" element={<Windows98DesktopInterface />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
