3dfef602342f8d753bc51432bfa8ef4ec55b9496
Mahan Tavakoli <94042573+MahanKenway@users.noreply.github.com>
2026-02-11 19:54:36 +0330

    Create index.jsx

---
+++ src/pages/windows-98-desktop-interface/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DesktopEnvironment from '../../components/DesktopEnvironment';
+
+const Windows98DesktopInterface = () => {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <DesktopEnvironment />
+    </div>
+  );
+};
+
+export default Windows98DesktopInterface;
