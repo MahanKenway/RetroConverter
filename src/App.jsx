07be83673ab4cb9afa36f68e8266f4127db86445
Mahan Tavakoli <94042573+MahanKenway@users.noreply.github.com>
2026-02-17 19:48:07 +0330

    Normalize gh-pages deployment workflow for current app updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Routes from "./Routes";
 import { Toaster } from 'react-hot-toast';
-import useGoogleAnalytics from './hooks/useGoogleAnalytics';
 
 function App() {
-  useGoogleAnalytics();
-
   return (
     <>
       <Toaster position="top-right" />
